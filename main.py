5131d3f6647dac9218cd2c61b11abcb718641716
unknown <TLpigff>
2024-11-11 23:09:18 +0800

    feat: update 连板天梯

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import json
 from datetime import datetime
 
 
+current_date = datetime.now()
+formatted_date = current_date.strftime('%Y%m%d')
+
 file_name = '复盘.csv'
 
 headers = {
@@ -72,7 +75,7 @@ def 个股涨跌():
 
 def wencai1(query,query_type):
     cookie='other_uid=Ths_iwencai_Xuangu_s1e2xtpqi8wty9vgts3up1aeklanhmsa; ta_random_userid=s9cfw1a6ol; cid=5b58f21b2099cb8b327c3c71a9581bb01726156949; PHPSESSID=ddabea4a55168ee404f6f19e9f636e0f; wencai_pc_version=1; v=AysXlKL2yaQYXxXC3FxA3wUgukQQQG-WuUMDbZ0sBqbBh0U6JRDPEskkk2au'
-    res = wencai.get(query=query, cookie=cookie, query_type=query_type)
+    res = wencai.get(loop=True,query=query, cookie=cookie, query_type=query_type)
     return res
 
 def convert_to_percentage(s):
@@ -102,19 +105,16 @@ def write_大盘指数():
         file.write(markdown_content)
 
 def write_行业板块():
-    current_date = datetime.now()
-    formatted_date = current_date.strftime('%Y%m%d')
-
-    markdown_content = "# 行业概念板块\n\n"
+    markdown_content = "# 热点行业概念\n\n"
     markdown_content += "## 行业流入前十\n\n"
-    markdown_content += "| 行业 | 净流入  |\n"
-    markdown_content += "| ---- | --- |\n"
+    markdown_content += "| 行业 | 净流入  | 涨停家数 |\n"
+    markdown_content += "| ---- | --- | --- | \n"
 
-    data = wencai1('二级行业板块主力净流入前十','zhishu')
+    data = wencai1('二级行业板块主力净流入前十，显示涨停家数','zhishu')
 
     for index,row in data.iterrows():
         money = "%.2f亿元" % (row['指数@主力资金流向[{}]'.format(formatted_date)] / 100000000)
-        markdown_content += f"| {row['指数简称']} | {money} |\n"
+        markdown_content += f"| {row['指数简称']} | {money} | {row['指数@涨停家数[{}]'.format(formatted_date)]} |\n"
 
     markdown_content += "## 行业流出前五\n\n"
     markdown_content += "| 行业 | 净流入  |\n"
@@ -127,14 +127,14 @@ def write_行业板块():
         markdown_content += f"| {row['指数简称']} | {money} |\n"
 
     markdown_content += "## 概念板块涨幅前十\n\n"
-    markdown_content += "| 概念名称 | 涨跌幅  |\n"
-    markdown_content += "| ---- | --- |\n"
+    markdown_content += "| 概念名称 | 涨跌幅  | 涨停家数 |\n"
+    markdown_content += "| ---- | --- | --- |\n"
 
-    data = wencai1('概念板块涨幅前十','zhishu')
+    data = wencai1('概念板块涨幅前十，显示涨停家数','zhishu')
 
     for index,row in data.iterrows():
         value = "%.2f%%" % (float(row['指数@涨跌幅:前复权[{}]'.format(formatted_date)]))
-        markdown_content += f"| {row['指数简称']} | {value} |\n"    
+        markdown_content += f"| {row['指数简称']} | {value} | {row['指数@涨停家数[{}]'.format(formatted_date)]} |\n"    
 
     markdown_content += "## 概念板块跌幅前五\n\n"
     markdown_content += "| 概念名称 | 涨跌幅  |\n"
@@ -146,31 +146,60 @@ def write_行业板块():
         value = "%.2f%%" % (float(row['指数@涨跌幅:前复权[{}]'.format(formatted_date)]))
         markdown_content += f"| {row['指数简称']} | {value} |\n"
 
+    markdown_content += "## 主力资金流入前十\n\n"
+    markdown_content += "| 股票名称 | 资金流入  |\n"
+    markdown_content += "| ---- | --- |\n"
+
+    data = wencai1('主力流入前十','stock')
+
+    for index,row in data.iterrows():
+        money = "%.2f亿元" % (row['主力资金流向[{}]'.format(formatted_date)] / 100000000)
+        markdown_content += f"| {row['股票简称']} | {money} |\n"    
+
+    markdown_content += "## 主力资金流出前十\n\n"
+    markdown_content += "| 股票名称 | 资金流出  |\n"
+    markdown_content += "| ---- | --- |\n"
+
+    data = wencai1('主力流出前十','stock')
+
+    for index,row in data.iterrows():
+        money = "%.2f亿元" % (row['主力资金流向[{}]'.format(formatted_date)] / 100000000)
+        markdown_content += f"| {row['股票简称']} | {money} |\n"        
+
     with open('output.md', 'a', encoding='utf-8') as file:
         file.write(markdown_content)    
 
 def write_连板天梯():
-    markdown_content = "# 连板天梯\n\n"
-    markdown_content += "| 连续涨停数 | 股票 | 涨停原因 |\n"
+    markdown_content = "# 涨停复盘\n\n"
+
+    chuangye_data = wencai1('今日创业板涨停,非ST','stock')
+    kechuang_data = wencai1('今日科创板涨停,非ST','stock')
+    beijiao_data = wencai1('今日北交所涨停,非ST','stock')
+    zhuban_data = wencai1('今日主板涨停,非ST','stock')
+
+    markdown_content += "今日创业板涨停{}家，".format(len(chuangye_data))
+    markdown_content += "科创板板涨停{}家，".format(len(kechuang_data))
+    markdown_content += "北交所涨停{}家，".format(len(beijiao_data))
+    markdown_content += "主板涨停{}家。".format(len(zhuban_data))
+
+    markdown_content += "## 连板天梯\n\n"
+    markdown_content += "| 连续涨停天数 | 股票名称  | 涨停原因 |\n"
     markdown_content += "| ---- | --- | --- |\n"
 
-    data = 连板天梯()
-    for stock in data['data']:
-        if 'ST' in stock['secu_name']:
-            continue
-        if stock['limit_up_days'] == 1: 
-            continue
-        markdown_content += f"| {stock['limit_up_days']} | {stock['secu_name'][0] + '*' + stock['secu_name'][2:]} | {stock['up_reason'].split('|')[0]} |\n"
+    data = wencai1('连板天梯，非ST，显示涨停原因','stock')
 
-    markdown_content += "# 今日首板\n\n"
-    markdown_content += "| 股票 | 涨停原因 |\n"
+    for index,row in data.iterrows():
+        markdown_content += f"| {row['连续涨停天数[{}]'.format(formatted_date)]} | {row['股票简称']} | {row['涨停原因类别[{}]'.format(formatted_date)]} |\n"  
+
+    markdown_content += "## 今日首板\n\n"
+    markdown_content += "| 股票名称  | 涨停原因 |\n"
     markdown_content += "| ---- | --- |\n"
-    for stock in data['data']:
-        if 'ST' in stock['secu_name']:
-            continue
-        if stock['limit_up_days'] > 1: 
-            continue
-        markdown_content += f"| {stock['secu_name'][0] + '*' + stock['secu_name'][2:]} | {stock['up_reason'].split('|')[0]} |\n"
+
+    data = wencai1('今日首板，非ST，按首次涨停时间排序，显示涨停原因','stock')
+
+    for index,row in data.iterrows():
+        markdown_content += f"| {row['股票简称']} | {row['涨停原因类别[{}]'.format(formatted_date)]} |\n"  
+
 
     with open('output.md', 'a', encoding='utf-8') as file:
         file.write(markdown_content)
@@ -183,7 +212,7 @@ def write_个股涨跌():
     markdown_content += "## 涨跌停详情\n\n"
     markdown_content += "涨停家数  " + str(up_down_data['up_num']) + "，"
     markdown_content += "跌停家数  " + str(up_down_data['down_num']) + '，'
-    markdown_content += "封板率  " + str(data['up_ratio']) + '，'
+    markdown_content += "封板率  " + str(data['up_ratio']) + '\n'
 
     markdown_content += "## 涨跌详情\n\n"
     markdown_content += "上涨家数  " + str(up_down_data['rise_num']) + '，'
@@ -191,21 +220,21 @@ def write_个股涨跌():
     markdown_content += "平盘家数  " + str(up_down_data['flat_num']) + '\n'
 
     markdown_content += "## 涨跌幅分布\n\n"
-    markdown_content += "| 幅度 | 涨幅超过 | 跌幅超过 | \n"
+    markdown_content += "| 幅度 | 涨幅 | 跌幅 | \n"
     markdown_content += "| ---- | --- | --- |\n"
-    markdown_content += f"| 2% | {str(up_down_data['up_2'])} | {str(up_down_data['down_2'])} |\n"
-    markdown_content += f"| 4% | {str(up_down_data['up_4'])} | {str(up_down_data['down_4'])} |\n"
-    markdown_content += f"| 6% | {str(up_down_data['up_6'])} | {str(up_down_data['down_6'])} |\n"
-    markdown_content += f"| 8% | {str(up_down_data['up_8'])} | {str(up_down_data['down_8'])} |\n"
-    markdown_content += f"| 10% | {str(up_down_data['up_10'])} | {str(up_down_data['down_10'])} |\n"
+    markdown_content += f"| 2%-4% | {str(up_down_data['up_2'])} | {str(up_down_data['down_2'])} |\n"
+    markdown_content += f"| 4%-6% | {str(up_down_data['up_4'])} | {str(up_down_data['down_4'])} |\n"
+    markdown_content += f"| 6%-8% | {str(up_down_data['up_6'])} | {str(up_down_data['down_6'])} |\n"
+    markdown_content += f"| 8%-10% | {str(up_down_data['up_8'])} | {str(up_down_data['down_8'])} |\n"
+    markdown_content += f"| 超过10% | {str(up_down_data['up_10'])} | {str(up_down_data['down_10'])} |\n"
 
     with open('output.md', 'a', encoding='utf-8') as file:
         file.write(markdown_content)
 
 def main():
     write_大盘指数()
-    write_行业板块()
     write_个股涨跌()
+    write_行业板块()
     write_连板天梯()
 
 if __name__ == "__main__":
